400bfbf33db58c30694053101a5328eb535cc57f
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-01 15:31:57 +0200

    feat: filmList, html-structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { useState } from "react";
 
+const filmList = [
+	{ title: "Inception", genre: "Fantascienza" },
+	{ title: "Il Padrino", genre: "Thriller" },
+	{ title: "Titanic", genre: "Romantico" },
+	{ title: "Batman", genre: "Azione" },
+	{ title: "Interstellar", genre: "Fantascienza" },
+	{ title: "Pulp Fiction", genre: "Thriller" },
+];
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-12"></div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
